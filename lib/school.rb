deea38eaf8df13ccefb522f4b96dcbb2fc6a3d45
Adam Kearney <aklives@hotmail.com>
2019-04-06 21:08:36 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,15 @@
-# code here!
+class School
+  
+  attr_accessor :school, :roster
+
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    @roster[grade] = []
+    @roster[grade] << student
+  end
+
+end
